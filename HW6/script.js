2a01885b87675b2866c8d8894bc5335c6dee8123
patabu <bu.davide00@gmail.com>
2024-11-12 16:38:36 +0100

    fix(hw6): funzionante con range di probabilità

--- HW6/script.js
+++ HW6/script.js
@@ -39,36 +39,21 @@ function drawChartAxis() {
     ctx.stroke();
 }
 
-function drawChartLines(probabilities, distributions, numberOfAttempts) {
-    const spacePerProbability = (graphPositions.xEnd - graphPositions.xStart) / distributions.length;
-    ctx.strokeStyle = "#9ca0a6";
-    ctx.lineWidth = .5;
-    for (let indexProbability = 1; indexProbability < distributions.length; indexProbability++) {
-        ctx.beginPath();
-        ctx.moveTo(graphPositions.xStart + (spacePerProbability * indexProbability), graphPositions.yStart);
-        ctx.lineTo(graphPositions.xStart + (spacePerProbability * indexProbability), graphPositions.yEnd);
-        ctx.stroke();
-    }
+function drawChartLines(probabilities, numberOfAttempts) {
+    const spacePerProbability = (graphPositions.xEnd - graphPositions.xStart) / probabilities.length;
 
-    const higherDistribution = Math.max(...distributions);    
+    const higherDistribution = Math.max(...(probabilities.map(probabilityData => [probabilityData.empiricDistribution, probabilityData.theoreticalDistribution])).flat());    
     const distributionPerSpace = higherDistribution / 10;
+    
     const graphHeight = graphPositions.yEnd - graphPositions.yStart;
     const spacePerDistribution = graphHeight / 10;
 
-    for (let horizontalRowIndex = 0; horizontalRowIndex < 10; horizontalRowIndex++) {
-        const y = graphPositions.yStart + (spacePerDistribution * horizontalRowIndex)
-        ctx.beginPath();
-        ctx.moveTo(graphPositions.xStart, y);
-        ctx.lineTo(graphPositions.xEnd, y);
-        ctx.stroke();
-        ctx.fillText((higherDistribution - (distributionPerSpace * horizontalRowIndex)).toFixed(2), graphPositions.xStart - 25, y);
-    }
+
 
     const padding = spacePerProbability / 20;
     const distributionWidth = (spacePerProbability - (3 * padding)) / 2;
-    for (let distributionIndex = 0; distributionIndex < distributions.length; distributionIndex++) {
+    for (let distributionIndex = 0; distributionIndex < probabilities.length; distributionIndex++) {
         // * Empiric
-        console.log(distributions[distributionIndex]);
         
         ctx.beginPath();
         ctx.fillStyle = "#1089e2"; 
@@ -76,7 +61,7 @@ function drawChartLines(probabilities, distributions, numberOfAttempts) {
             (graphPositions.xStart + (distributionIndex * spacePerProbability)) + padding,
             graphPositions.yEnd,
             distributionWidth,
-            -((distributions[distributionIndex] / higherDistribution) * graphHeight)
+            -((probabilities[distributionIndex].empiricDistribution / higherDistribution) * graphHeight)
         );
         ctx.fill();
         // * Theoretical
@@ -86,88 +71,53 @@ function drawChartLines(probabilities, distributions, numberOfAttempts) {
             (graphPositions.xStart + (distributionIndex * spacePerProbability)) + (2 * padding) + distributionWidth,
             graphPositions.yEnd,
             distributionWidth,
-            -(((probabilities[distributionIndex] * numberOfAttempts) / higherDistribution) * graphHeight)
+            -((probabilities[distributionIndex].theoreticalDistribution / higherDistribution) * graphHeight)
         );
         ctx.fill(); 
-
     }
 
-
-
     ctx.strokeStyle = "#000000";
-}
+    ctx.fillStyle = "#000000";
 
-// * La probabilità definisce un salto di +- sqrt(1/numberOfIntervals)
+    ctx.strokeStyle = "#9ca0a6";
+    ctx.lineWidth = .5;
+    for (let indexProbability = 1; indexProbability < probabilities.length; indexProbability++) {
+        ctx.beginPath();
+        ctx.moveTo(graphPositions.xStart + (spacePerProbability * indexProbability), graphPositions.yStart);
+        ctx.lineTo(graphPositions.xStart + (spacePerProbability * indexProbability), graphPositions.yEnd);
+        ctx.stroke();
+    }
+
+    for (let horizontalRowIndex = 0; horizontalRowIndex < 10; horizontalRowIndex++) {
+        const y = graphPositions.yStart + (spacePerDistribution * horizontalRowIndex)
+        ctx.beginPath();
+        ctx.moveTo(graphPositions.xStart, y);
+        ctx.lineTo(graphPositions.xEnd, y);
+        ctx.stroke();
+        ctx.fillText((higherDistribution - (distributionPerSpace * horizontalRowIndex)).toFixed(2), graphPositions.xStart - 35, y);
+    }
+
+}
 
 function onExecute() {
     try {
-        // const { numberOfIntervals, numberOfAttackers, probability } = getInputData();
-        const {probabilities, numberOfAttempts} = getInputData();
+        const {probabilitiesData, numberOfAttempts} = getInputData();
         clearChart();
         drawChartAxis();
-        const distributions = new Array(probabilities.length).fill(0);
-        for (let probabilityIndex = 0; probabilityIndex < probabilities.length; probabilityIndex++) {
+        for (let probabilityIndex = 0; probabilityIndex < probabilitiesData.length; probabilityIndex++) {
+            const probabilityElement = probabilitiesData[probabilityIndex]
+            console.log('Probability n.' + (probabilityIndex + 1) + " Range " + probabilityElement.startProbability + " - " + probabilityElement.endProbability);
             for (let attempt = 1; attempt <= numberOfAttempts; attempt++) {
-                if (Math.random() <= probabilities[probabilityIndex]) distributions[probabilityIndex]++;
+                
+                const probability = Math.random();
+                if (probability >= probabilityElement.startProbability && probability < probabilityElement.endProbability) {
+                    console.log('Success');
+                    probabilityElement.empiricDistribution++;
+                }
             }
+            console.log(probabilityElement.empiricDistribution + ' successful attempts');
         }
-        drawChartLines(probabilities, distributions, 50);
-        // ctx.font = "12px serif";
-        // const spacePerServerXAxis = (graphPositions.xEnd - graphPositions.xStart) / numberOfIntervals;
-    
-        // let penetratedServers = new Array((numberOfIntervals * 2) + 1).fill(0);
-    
-        // ctx.textAlign = "center";
-        // ctx.fillText(0, graphPositions.xStart - 10, graphPositions.yStartingPoint + 5);
-        // ctx.fillText(Math.sqrt(numberOfIntervals).toFixed(2), graphPositions.xStart, graphPositions.yStart - 5)
-        // ctx.fillText(-Math.sqrt(numberOfIntervals).toFixed(2), graphPositions.xStart, graphPositions.yEnd + 15)
-        // ctx.fillText(numberOfIntervals, graphPositions.xEnd, graphPositions.yEnd + 20)
-        
-        // const spacePerServerYAxis = (graphPositions.yEnd - graphPositions.yStart) / (numberOfIntervals * 2);
-        // ctx.lineWidth = 1.5;
-    
-    
-        // for (let attacker = 0; attacker < numberOfAttackers; attacker++) {
-        //     let numberOfSuccessfulAttacks = numberOfIntervals;
-        //     ctx.strokeStyle = "#" + Math.floor(Math.random()*16777215).toString(16);
-        //     for (let server = 0; server < numberOfIntervals; server++) {
-        //         const xStartPoint = graphPositions.xStart + (spacePerServerXAxis * server);
-        //         const xEndPoint = graphPositions.xStart + (spacePerServerXAxis * (server + 1))
-        //         const yStartPoint = (graphPositions.yStartingPoint + (spacePerServerYAxis * (numberOfIntervals - numberOfSuccessfulAttacks))); // * Qui viene eseguito il calcolo per l'Y di partenza
-        //         numberOfSuccessfulAttacks += Math.random() <= probability ? 1 : -1;
-        //         const yEndPoint = (graphPositions.yStartingPoint + (spacePerServerYAxis * (numberOfIntervals - numberOfSuccessfulAttacks)));
-        //         ctx.beginPath();
-        //         ctx.moveTo(xStartPoint, yStartPoint);
-        //         ctx.lineTo(xEndPoint, yEndPoint);
-        //         ctx.stroke();
-        //     }      
-        //     penetratedServers[numberOfSuccessfulAttacks]++;             
-        // }
-        // console.log(penetratedServers);
-    
-        // ctx.strokeStyle = "#000000";
-        // const maxHackersOnSameServer = Math.max(...penetratedServers);
-        // const spaceForHackerDistribution = ((canvas.width * 1 / 5) - 20) / maxHackersOnSameServer;
-        // for (let server = (numberOfIntervals * 2) + 1; server >= 0; server--) {
-        //     if (!penetratedServers[server]) continue;
-        //     const xStartPoint = graphPositions.xEnd;
-        //     const xEndPoint = xStartPoint + (spaceForHackerDistribution * penetratedServers[server]);
-        //     const y = graphPositions.yStart + (((graphPositions.yEnd - graphPositions.yStart) / ((numberOfIntervals * 2) + 1)) * ((numberOfIntervals * 2) - server));
-        //     ctx.strokeStyle = "#ff0000";
-        //     ctx.beginPath();
-        //     ctx.moveTo(xStartPoint, y);
-        //     ctx.lineTo(xEndPoint, y);
-        //     ctx.stroke();
-        // }
-    
-        // ctx.strokeStyle = "#000000";
-        // ctx.beginPath();
-        // ctx.setLineDash([5, 5]);
-        // ctx.moveTo(canvas.width - 20, graphPositions.yEnd);
-        // ctx.lineTo(canvas.width - 20, graphPositions.yStart);
-        // ctx.stroke();
-        // ctx.fillText(maxHackersOnSameServer, canvas.width - 20, graphPositions.yEnd + 20);
-        // ctx.setLineDash([]);
+        drawChartLines(probabilitiesData, 50);
     } catch (error) {
         console.log(error);
         window.alert(error);
@@ -181,9 +131,6 @@ function generateProbabilities() { // * Le probabilità devono essere intervalli
         const inputsContainer = document.getElementById("input-dynamic-container");
         inputsContainer.innerHTML = "";
         const isProbabilityRandom = (document.getElementById("randomProbabilities")).checked;
-        console.log(isProbabilityRandom);
-        
-
         if (isProbabilityRandom) generateRandomProbabilities(numberOfProbabilities, inputsContainer);
         else generateCloseProbabilities(numberOfProbabilities, inputsContainer);
     } catch (error) {
@@ -229,15 +176,20 @@ function createProbabilityInput(inputIndex, inputsContainer, probability) {
 function getInputData() {
     const numberOfProbabilites = Number(document.getElementById("numberOfProbabilities").value);
     const numberOfAttempts = Number(document.getElementById("numberOfAttempts").value);
-    const probabilities = [];
+    const probabilitiesData = [];
     let totalProbability = 0;
     for (let inputIndex = 1; inputIndex <= numberOfProbabilites; inputIndex++) {
         const probabilityInput = document.getElementById("PR" + inputIndex);
         const probability = Number(probabilityInput.value);
         if (isNaN(probability) || probability < 0 || probability > 1) throw Error("Probability must be between 0 and 1");
-        probabilities.push(probability);
+        probabilitiesData.push({
+            startProbability: totalProbability,
+            endProbability: totalProbability + probability - 0.001,
+            empiricDistribution: 0,
+            theoreticalDistribution: probability * numberOfAttempts
+        });
         totalProbability += probability;
     }
     if (totalProbability > 1) throw Error("Sum of probabilities must be less than 1");
-    return {probabilities, numberOfAttempts};
+    return {probabilitiesData, numberOfAttempts};
 }
