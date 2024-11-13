f3c11331b1d36fa7147730051667952a2afd34c6
patabu <bu.davide00@gmail.com>
2024-11-13 10:04:17 +0100

    feat(hw6): aggiunta research, tweaks esercizio pratico

--- HW6/script.js
+++ HW6/script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+let areProbabilitiesGenerated = false;
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 200;
@@ -37,9 +38,35 @@ function drawChartAxis() {
     ctx.moveTo(graphPositions.xStart, graphPositions.yEnd);
     ctx.lineTo(graphPositions.xEnd, graphPositions.yEnd);
     ctx.stroke();
+
+    // * Draw legend 20x5 rect
+    ctx.fillStyle = "#329e11";
+    ctx.fillRect(
+        ((graphPositions.xEnd - graphPositions.xStart) / 2) + graphPositions.xStart - 150,
+        graphPositions.yStart - 30,
+        60,
+        10
+    );
+    ctx.fill();
+
+    ctx.font = "14px serif";
+    ctx.fillStyle = "#000000";
+    ctx.fillText('Empiric', ((graphPositions.xEnd - graphPositions.xStart) / 2) + graphPositions.xStart - 80, graphPositions.yStart - 20)
+
+    ctx.fillStyle = "#78ad86";
+    ctx.fillRect(
+        ((graphPositions.xEnd - graphPositions.xStart) / 2) + graphPositions.xStart + 50,
+        graphPositions.yStart - 30,
+        60,
+        10
+    );
+    ctx.fill();
+
+    ctx.fillStyle = "#000000";
+    ctx.fillText('Theoretical', ((graphPositions.xEnd - graphPositions.xStart) / 2) + graphPositions.xStart + 120, graphPositions.yStart - 20)
 }
 
-function drawChartLines(probabilities, numberOfAttempts) {
+function drawChartLines(probabilities) {
     const spacePerProbability = (graphPositions.xEnd - graphPositions.xStart) / probabilities.length;
 
     const higherDistribution = Math.max(...(probabilities.map(probabilityData => [probabilityData.empiricDistribution, probabilityData.theoreticalDistribution])).flat());    
@@ -48,28 +75,26 @@ function drawChartLines(probabilities, numberOfAttempts) {
     const graphHeight = graphPositions.yEnd - graphPositions.yStart;
     const spacePerDistribution = graphHeight / 10;
 
-
-
     const padding = spacePerProbability / 20;
     const distributionWidth = (spacePerProbability - (3 * padding)) / 2;
     for (let distributionIndex = 0; distributionIndex < probabilities.length; distributionIndex++) {
         // * Empiric
         
         ctx.beginPath();
-        ctx.fillStyle = "#1089e2"; 
+        ctx.fillStyle = "#329e11"; 
         ctx.rect(
             (graphPositions.xStart + (distributionIndex * spacePerProbability)) + padding,
-            graphPositions.yEnd,
+            graphPositions.yEnd - 1,
             distributionWidth,
             -((probabilities[distributionIndex].empiricDistribution / higherDistribution) * graphHeight)
         );
         ctx.fill();
         // * Theoretical
-        ctx.fillStyle = "#53ef21"
+        ctx.fillStyle = "#78ad86";
         ctx.beginPath();
         ctx.rect(
             (graphPositions.xStart + (distributionIndex * spacePerProbability)) + (2 * padding) + distributionWidth,
-            graphPositions.yEnd,
+            graphPositions.yEnd - 1,
             distributionWidth,
             -((probabilities[distributionIndex].theoreticalDistribution / higherDistribution) * graphHeight)
         );
@@ -101,60 +126,50 @@ function drawChartLines(probabilities, numberOfAttempts) {
 
 function onExecute() {
     try {
-        const {probabilitiesData, numberOfAttempts} = getInputData();
+        const { probabilitiesData, numberOfAttempts } = getInputData();
         clearChart();
         drawChartAxis();
         for (let probabilityIndex = 0; probabilityIndex < probabilitiesData.length; probabilityIndex++) {
             const probabilityElement = probabilitiesData[probabilityIndex]
             console.log('Probability n.' + (probabilityIndex + 1) + " Range " + probabilityElement.startProbability + " - " + probabilityElement.endProbability);
             for (let attempt = 1; attempt <= numberOfAttempts; attempt++) {
-                
-                const probability = Math.random();
-                if (probability >= probabilityElement.startProbability && probability < probabilityElement.endProbability) {
+                const probability = toFixedNumber(Math.random());
+                if (probability > probabilityElement.startProbability && probability <= probabilityElement.endProbability) {
                     console.log('Success');
                     probabilityElement.empiricDistribution++;
                 }
             }
             console.log(probabilityElement.empiricDistribution + ' successful attempts');
         }
-        drawChartLines(probabilitiesData, 50);
+        drawChartLines(probabilitiesData);
     } catch (error) {
         console.log(error);
         window.alert(error);
     }
 }
 
-function generateProbabilities() { // * Le probabilità devono essere intervalli!!! 0 - 0.2 / 0.2 - 0.35 / 0.35 - 0.6 ...
+function generateProbabilities() {
     try {
+        areProbabilitiesGenerated = true;
+        (document.getElementById("executeButton")).style.display = 'block';
         const numberOfProbabilities = Number(document.getElementById("numberOfProbabilities").value);
         if (isNaN(numberOfProbabilities) || numberOfProbabilities < 1 || numberOfProbabilities > 20) throw Error("Number of probabilites must be between 1 and 20");
         const inputsContainer = document.getElementById("input-dynamic-container");
         inputsContainer.innerHTML = "";
-        const isProbabilityRandom = (document.getElementById("randomProbabilities")).checked;
-        if (isProbabilityRandom) generateRandomProbabilities(numberOfProbabilities, inputsContainer);
-        else generateCloseProbabilities(numberOfProbabilities, inputsContainer);
+        generateCloseProbabilities(numberOfProbabilities, inputsContainer);
     } catch (error) {
         console.log(error);
         window.alert(error);
     }
 }
 
-function generateRandomProbabilities(numberOfProbabilities, inputsContainer) {
-    let max = 1;
-    for (let inputIndex = 1; inputIndex <= numberOfProbabilities; inputIndex++) {
-        const probability = Math.random() * max;
-        max -= probability;
-        createProbabilityInput(inputIndex, inputsContainer, probability);
-    }
-}
-
 function generateCloseProbabilities(numberOfProbabilities, inputsContainer) {
-    const equalProbability = 1 / numberOfProbabilities;
+    const equalProbability = toFixedNumber(1 / numberOfProbabilities);
     const lastCoupleIndex = numberOfProbabilities % 2 === 0 ? numberOfProbabilities : numberOfProbabilities - 1;
     for (let inputIndex = 1; inputIndex <= lastCoupleIndex; inputIndex++) {
-        const offset = Math.random() * (equalProbability / 2);
-        createProbabilityInput(inputIndex, inputsContainer, equalProbability + offset);
-        createProbabilityInput(++inputIndex, inputsContainer, equalProbability - offset);
+        const offset = toFixedNumber(Math.random() * (equalProbability / 2));
+        createProbabilityInput(inputIndex, inputsContainer, toFixedNumber(equalProbability + offset));
+        createProbabilityInput(++inputIndex, inputsContainer, toFixedNumber(equalProbability - offset));
     }
     if (numberOfProbabilities % 2 !== 0) createProbabilityInput(numberOfProbabilities, inputsContainer, equalProbability);
 }
@@ -181,15 +196,19 @@ function getInputData() {
     for (let inputIndex = 1; inputIndex <= numberOfProbabilites; inputIndex++) {
         const probabilityInput = document.getElementById("PR" + inputIndex);
         const probability = Number(probabilityInput.value);
-        if (isNaN(probability) || probability < 0 || probability > 1) throw Error("Probability must be between 0 and 1");
+        if (isNaN(probability) || probability <= 0 || probability > 1) throw Error("Probability must be between 0 and 1");
         probabilitiesData.push({
             startProbability: totalProbability,
-            endProbability: totalProbability + probability - 0.001,
+            endProbability: inputIndex === numberOfProbabilites ? 1 : totalProbability + probability,
             empiricDistribution: 0,
             theoreticalDistribution: probability * numberOfAttempts
         });
-        totalProbability += probability;
+        totalProbability = totalProbability + probability;
     }
-    if (totalProbability > 1) throw Error("Sum of probabilities must be less than 1");
     return {probabilitiesData, numberOfAttempts};
+}
+
+function toFixedNumber(num, digits = 3, base = 10){
+    const pow = Math.pow(base, digits);
+    return Math.round(num * pow) / pow;
 }
